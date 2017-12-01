c83670198daed8a1568a4195f10143fad486fdaf
rickyahn0612 <rickyahn0612@yahoo.com>
2017-12-01 14:35:31 -0800

    adding minor animations to coming page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+
+$(document).ready(function (e) {
+	$('html, body').fadeIn(2000);
+  $('.symbol').animate({'opacity': 1, top: 0}, 2000, function(){
+    $('.words').animate({'opacity': 1, top: 0}, 1000, function(){
+
+      $('.coming-soon-txt').fadeIn(1000);
+    })
+
+  })
+});
