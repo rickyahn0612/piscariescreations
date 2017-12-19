9a8df80937eeda1f92319985f66ffc465c407769
rickyahn0612 <rickyahn0612@yahoo.com>
2017-12-18 16:45:53 -0800

    homepage layout

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,2 @@
 //= require_tree .
 
-$(document).ready(function (e) {
-	$('html, body').fadeIn(2000);
-  $('.symbol').animate({'opacity': 1, top: 0}, 2000, function(){
-    $('.words').animate({'opacity': 1, top: 0}, 1000, function(){
-
-      $('.coming-soon-txt, span, a').fadeIn(2000);
-    })
-
-  })
-});
